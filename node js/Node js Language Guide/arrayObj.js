3ba76d49232c7109b49def67f6e9071f6cd394f4
김건형 <show11837@gmail.com>
2020-07-02 18:09:33 +0900

    feat : findCar(carname) 함수 추가 - 차가 배열에 있는지 확인

--- node js/Node js Language Guide/arrayObj.js
+++ node js/Node js Language Guide/arrayObj.js
@@ -32,8 +32,14 @@ var car = {
   var cars = [car, car2, car3];
 
   //#work2 자동차 배열을 확인한 뒤 bmw 라는 자동차가 있으면 hello 출력해주세요 for / if 활용
-function findCar(carname) {
-    
+function findCar(carname) { 
+    for (var i = 0; i < cars.length; i++) {
+        var car = cars[i];
+        if (car.name == carname) {
+            console.log("find!");
+        }
+    }
 }
 
-  console.log(cars);
+console.log(cars);
+findCar("bmw");
