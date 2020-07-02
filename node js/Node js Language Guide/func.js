c0a40fd09d5372e7512ff7dd6b3518d49eb85552
김건형 <show11837@gmail.com>
2020-07-02 15:40:24 +0900

    feat : func.js (함수)

---
+++ node js/Node js Language Guide/func.js
@@ -0,0 +1,23 @@
+function newFunc(p1, p2) {
+    console.log(p1);
+    console.log(p2);
+    // ----- ,,,,,,
+    // ,,,,
+
+    return p1
+}
+
+function multi(p1, p2) {
+    return p1 * p2; // p1, p2 곱연산의 결과를 반환
+}
+
+// #work1 + - * / 기능 작성 및 결과 출력
+function arithmetic(p1, p2) {
+    console.log(p1 + p2);
+    console.log(p1 - p2);
+    console.log(p1 * p2);
+    console.log(p2 / p2);
+}
+
+newFunc(1, 2)
+arithmetic(5, 3)
