cc548de9ba0726f996ab445a1098cbd278340f75
김건형 <show11837@gmail.com>
2020-07-07 10:47:43 +0900

    feat : 콜백 함수

---
+++ node js/Node js Language Guide/callback.js
@@ -0,0 +1,28 @@
+function aFunc(callback) {
+    setTimeout(function () {
+        console.log("a");
+        callback();
+    }, 100);
+}
+
+function bFunc(callback) {
+    setTimeout(function () {
+        console.log("b");
+        callback();
+    }, 1000);
+}
+
+function cFunc(callback) {
+    setTimeout(function () {
+        console.log("c");
+        callback();
+    }, 500);
+}
+
+// aFunc(bFunc(cFunc));
+
+aFunc(function () {
+    bFunc(function () {
+        cFunc(function () {}); // Promise, Async awit
+    })
+});
