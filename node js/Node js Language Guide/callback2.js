a9823c0515055d56098b8d2e3b456dc548238848
김건형 <show11837@gmail.com>
2020-07-07 11:08:15 +0900

    style : 불필요한 설명 제거

--- node js/Node js Language Guide/callback2.js
+++ node js/Node js Language Guide/callback2.js
@@ -1,17 +1,28 @@
+/*
+좋은 예재는 아닙니다.
+목적 : callback 함수에서 데이터를 어떻게 전달이 되는지를 확인한다.
+*/
+
 var fs = require('fs');
 
-console.log("first func");
-var data = "none";
+console.log("start");
+var readData = "none";
 
-fs.readFile("./example/test.txt", "utf8", function (err, result){  // callback 함수
-    if (err) {
-        console.error(err);
-        throw err;
-    } else {
-        data = result;
-        console.log(data);
-    }
-});
+function callbackFunc(callback) {
+    fs.readFile("./example/test.txt", "utf8", function (err, result){  // callback 함수
+        if (err) {
+            console.error(err);
+            throw err;
+        } else {
+            data = result;
+            callback(readData)
+        }
+    });
+}
 
-console.log(data);
-console.log("third func");
+console.log("A");
+
+callbackFunc(function (data){ // 다른 라이브러리를 사용할 때 이러한 유형을 많이 보게 될 것이다.
+    console.log(data);
+    console.log("C");
+})
