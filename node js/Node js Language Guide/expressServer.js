44cd27354a59060cbc158efaab727852ea913b7e
김건형 <show11837@gmail.com>
2020-07-09 15:59:28 +0900

    doc : 주석을 통한 설명 추가

--- node js/Node js Language Guide/expressServer.js
+++ node js/Node js Language Guide/expressServer.js
@@ -1,29 +1,43 @@
 const express = require("express");
 const app = express();
 
-app.set("views", __dirname + "/views");
-app.set("view engine", "ejs");
+app.set("views", __dirname + "/views"); 
+app.set("view engine", "ejs"); // ejs 사용 코드 추가
+
+app.use(express.json()); // // 리퀘스트 바디 허용 코드 추가
+app.use(express.urlencoded({ extended: false })); // data를 받아 올수 있도록 허용
 
 app.get("/", function (req, res) {
-    res.send("Hello World");
+  res.send("Hello World");
 });
 
 app.get("/hello", function (req, res) {
-    res.send("안녕하세요 !");
+  res.send("안녕하세요 !");
 });
 
 app.get("/htmlTest", function (req, res) {
-    res.send("<html><h1>안녕하세요</h1><hr/><p>만들기 어렵습니다</p></html>");
+  res.send("<html><h1>안녕하세요</h1><hr/><p>만들기 어렵습니다</p></html>");
 });
 
-app.get("/ejsTest", function (req, res) {
-    res.render("test");
+app.get("/ejsTest", function (req, res) { // ejs 뷰파일 및 라우터 추가
+  res.render("test");
 });
 
 app.get("/inputTest", function (req, res) {
-    res.render("inputTest");
+  res.render("inputTest");
+});
+
+// 비동기 통신 - 서버 프론트 통신 ajax
+// 로그인 정보 전달 ajax 코드 및 라우터 완성
+app.post("/getLoginData", function (req, res) {
+  var userId = req.body.ajaxUserId;
+  var userPassword = req.body.ajaxUserPassword;
+  console.log("req body : ", req.body); // 리퀘스트 바디 허용 코드 추가
+  console.log(userId, userPassword);
+
+  res.json(userId + "분의 로그인 성공입니다."); // 서버 프론트 통신 ajax 완성
 });
 
 app.listen(3000, function () {
-    console.log("Example app listening at http://localhost:3000");
+  console.log("Example app listening at http://localhost:3000");
 });
