af4a43b7ad96a5d5eb1bd419d6d1738ff5c7ef5d
김건형 <show11837@gmail.com>
2020-07-14 16:28:24 +0900

    feat : 로그인 페이지 추가

--- node js/Node js Language Guide/expressServer.js
+++ node js/Node js Language Guide/expressServer.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const request = require("request"); // 모듈 import, request 가져오기
+const jwt = require('jsonwebtoken');
+const auth = require('./lib/auth');
 
 var mysql = require('mysql'); // DB 연동
 var connection = mysql.createConnection({
@@ -17,7 +19,6 @@ app.set("view engine", "ejs"); // ejs 사용 코드 추가
 
 app.use(express.json()); // // 리퀘스트 바디 허용 코드 추가
 app.use(express.urlencoded({ extended: false })); // data를 받아 올수 있도록 허용
-
 app.use(express.static(__dirname + '/public')); // public이라는 folder를 외부에 공개 하겠다
 
 app.get("/", function (req, res) {
@@ -46,6 +47,14 @@ app.get("/designTest", function (req, res) {
 
 app.get("/signup", function (req, res) { // 오픈 뱅킹 API 로그인
   res.render("signup");
+});
+
+app.get("/login", function (req, res) {
+  res.render("login");
+});
+
+app.get("/authTest", auth, function (req, res) {
+  res.json("환영합니다. 우리 고객님")
 });
 
 app.get("/authResult", function (req, res) { // 인증 결과
@@ -99,13 +108,52 @@ app.post("/signup", function (req, res) {
   var sql =
     "INSERT INTO `user` (`name`, `email`, `password`, `accesstoken`, `refreshtoken`, `userseqno`) VALUES (?, ?, ?, ?, ?, ?)";
   connection.query(sql, [userName, userEmail, userPassword, userAccessToken, userRefreshToken, userSeqNo,], function (error, results, fields) {
-      if (error) throw error;
-      else {
-        console.log("sql :", this.sql);
-        res.json(1);
+    if (error) throw error;
+    else {
+      console.log("sql :", this.sql);
+      res.json(1);
+    }
+  }
+  );
+});
+
+app.post("/login", function (req, res) {
+  console.log("사용자 입력정보 :", req.body);
+  var userEmail = req.body.userEmail;
+  var userPassword = req.body.userPassword;
+  var sql = "SELECT * FROM user WHERE email = ?";
+  connection.query(sql, [userEmail], function (error, results, fields) {
+    if (error) throw error;
+    else {
+      if (results.length == 0) {
+        res.json("등록되지 않은 아이디 입니다.");
+      } else {
+        var dbPassword = results[0].password;
+        if (userPassword == dbPassword) {
+          res.json("로그인 성공");
+          var tokenKey = "fintech";
+          jwt.sign(
+            {
+              userId: results[0].id,
+              userEmail: results[0].email,
+            },
+            tokenKey,
+            {
+              expiresIn: "10d",
+              issuer: "fintech.admin",
+              subject: "user.login.info",
+            },
+            function (err, token) {
+              console.log("로그인 성공", token);
+              res.json(token);
+            }
+          );
+        } else {
+          res.json("비밀번호가 다릅니다!");
+        }
       }
     }
-  );
+  });
 });
 
 app.listen(3000, function () {
